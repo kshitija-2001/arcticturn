122e8cbf076213f3e0686f0b4a2b4c25c618eb50
kshitija-2001 <60540365+kshitija-2001@users.noreply.github.com>
2021-07-18 18:10:02 +0530

    commit

--- task.php
+++ task.php
@@ -1,28 +1,30 @@
 <?php include('Connect.php') ?>
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
 <head>
   <title>Home page</title>
   <link rel="stylesheet" href="css/task.css">
   <link rel="stylesheet" href="css/home.css">
   <script defer src="https://use.fontawesome.com/releases/v5.15.3/js/all.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 
 </head>
-  <body>
+
+<body>
   <div class="wrapper">
-            <nav> <input type="checkbox" id="show-search"> <input type="checkbox" id="show-menu"> <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
-                <div class="content">
-                    
-                    <div class="logo"><a href="home.php"><img src="images/logo/logo.png"></a></div>
-                    <ul class="links">
-                    <?php 
-                    
-                    echo'
+    <nav> <input type="checkbox" id="show-search"> <input type="checkbox" id="show-menu"> <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
+      <div class="content">
+
+        <div class="logo"><a href="home.php"><img src="images/logo/logo.png"></a></div>
+        <ul class="links">
+          <?php
+
+          echo '
                        <li><a href="home.php">Home</a></li>
                         <li><a href="travel.php">Travel</a></li>
                         <li> <a class="desktop-link">Relax</a> <input type="checkbox" id="show-services"> <label for="show-services">Services</label>
@@ -33,7 +35,7 @@
                             </ul>
                         </li>
                         <li><a href="organise.php">Organise</a></li>
-                        <li><a href="music.php">Music</a></li>
+                        
                         <li> <a class="desktop-link">Blogs</a> <input type="checkbox" id="show-services"> <label for="show-services">Services</label>
                             <ul>
                                 <li><a href="travelblog.php"><i class="fa fa-fw fa-helicopter"></i>Travel Blogs</a></li>
@@ -42,37 +44,40 @@
                             </ul>
                         </li>
 
-                    </ul>';?>
-                </div> <label for="show-search" class="search-icon"><i class="fas fa-search"></i></label>
-                <form action="" class="search-box"> <input type="text" placeholder="Type Something to Search..." required> <button type="submit" class="go-icon"><i class="fas fa-long-arrow-alt-right"></i></button> </form>
-                <a href="logout.php" type="button" class="btn btn-warning"><h5>Logout</h5></a>
+                    </ul>'; ?>
+      </div> <label for="show-search" class="search-icon"><i class="fas fa-search"></i></label>
+      <form action="" class="search-box"> <input type="text" placeholder="Type Something to Search..." required> <button type="submit" class="go-icon"><i class="fas fa-long-arrow-alt-right"></i></button> </form>
+      <a href="logout.php" type="button" class="btn btn-warning">
+        <h5>Logout</h5>
+      </a>
 
-            </nav>
-            </div>
-        </div>
-<div class="main-container">
-  <div class="card-container">
-  <?php
-   
-        $result = mysqli_query($Connect,"SELECT * FROM `tasks` WHERE `sdg`='UNSDG3'");
-					$i=1;
-          $x=1;
-					while($row = mysqli_fetch_array($result)) {
-				?>
-    <?php echo'<div class="card card-'.$x.'">';?>
-      <h2 class="card__title"><?php echo $row['description']?></h2>
-    </div>
-    <?php 
-    $x++;
-    if($x>5){
-      $x=1;
-    }
-     $i++;
-				}
-				?>
-   
-    
+    </nav>
   </div>
-</div>
-  </body>
+  </div>
+  <div class="main-container">
+    <div class="card-container">
+      <?php
+
+      $result = mysqli_query($Connect, "SELECT * FROM `tasks` WHERE `sdg`='UNSDG3'");
+      $i = 1;
+      $x = 1;
+      while ($row = mysqli_fetch_array($result)) {
+      ?>
+        <?php echo '<div class="card card-' . $x . '">'; ?>
+        <h2 class="card__title"><?php echo $row['description'] ?></h2>
+    </div>
+  <?php
+        $x++;
+        if ($x > 5) {
+          $x = 1;
+        }
+        $i++;
+      }
+  ?>
+
+
+  </div>
+  </div>
+</body>
+
 </html>
